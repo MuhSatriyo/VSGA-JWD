a44a2bcbfa87022ac8e4d1db8babf645c6df92fa
MuhSatriyo <muhamadsatriyo05@gmail.com>
2024-05-29 09:40:53 +0700

    For VSGA

---
+++ tugas/proses_hitung.php
@@ -0,0 +1,7 @@
+<?php
+// Mengambil data dari method POST
+$namaSiswa = $_POST['nama'];
+$berat = $_POST['berat'];
+$tinggi = $_POST['tinggi'];
+
+?>
---
+++ tugas/tugas_satu.php
@@ -0,0 +1,10 @@
+<?php
+echo "<div style='font-size: 24px;'>
+    Nama : Muhammad Satriyo Yuwono <br>
+    Asal Kampus : Universitas Sultan Ageng Tirtayasa <br>
+    Jurusan : Teknik Elektro <br>
+    Kota : Pringsewu - Lampung <br>
+    PHP : Baru mulai belajar <br>
+    HTML : Sudah Pernah
+</div>";
+?>
